cfc3869f484d965c742df6f94e98e7a82792e423
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2017-11-07 00:41:43 +0000

    test runner upgrades ;)

--- assert.hpp
+++ assert.hpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 class assert {
+
+
   public:
     static void Equal(const char* testName, int expected, int actual){
+      const char* failMark = "\033[31m✗\033[0m";
+      const char* passMark = "\033[32m✓\033[0m";
       if (expected != actual) {
-        std::cout << "x - " << testName << " (expected " << expected << ", actual " << actual << ") " << std::endl;
+        std::cout << failMark << " - " << testName << " (expected " << expected << ", actual " << actual << ") " << std::endl;
+      } else {
+        std::cout << passMark << " - " << testName << std::endl;
       }
     }
 };
